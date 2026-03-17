026e8d31320993b36940b231281b4cb5eef1654b
baiwumm <me@baiwumm.com>
2026-03-17 17:54:59 +0800

    feat: 引入 Nuxt UI

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <UButton>Button</UButton>
+    <h1 class="font-black text-2xl">哈哈哈</h1>
+    <span class="text-2xl text-primary font-black">Primary</span>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.baiwumm.com/fonts/MapleMono-CN-Regular/result.css',
+        },
+      ],
+    },
+  },
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  ui: {
+    fonts: false
+  }
 })

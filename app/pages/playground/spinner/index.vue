61a5efd974aa455ed3e02fe4ccb28d113a15a6a8
baiwumm <me@baiwumm.com>
2026-06-24 16:21:13 +0800

    style(spinner): 样式调整

--- app/pages/playground/spinner/index.vue
+++ app/pages/playground/spinner/index.vue
@@ -5,18 +5,10 @@ const sizes = ['sm', 'md', 'lg', 'xl'] as const
 
 <template>
   <div class="space-y-4">
-    <UAlert
-      :title="$t('pages.playground.spinner.description')"
-      color="neutral"
-      variant="outline"
-      icon="simple-icons:heroui"
-      :actions="[
-        {
-          label: 'Hero UI',
-          trailingIcon: 'lucide:external-link',
-          to: 'https://heroui.com',
-          target: '_blank',
-        },
+    <UPageHero
+      title="Spinner"
+      :description="$t('pages.playground.spinner.description')"
+      :links="[
         {
           label: 'Spinner',
           color: 'neutral',
@@ -25,16 +17,30 @@ const sizes = ['sm', 'md', 'lg', 'xl'] as const
           to: 'https://heroui.com/docs/react/components/spinner',
           target: '_blank',
         },
+        {
+          label: 'Hero UI',
+          icon: 'simple-icons:github',
+          to: 'https://github.com/heroui-inc/heroui',
+          target: '_blank',
+          trailingIcon: 'i-lucide-arrow-right',
+        },
       ]"
-    />
-    <UCard title="Usage" :ui="{ body: 'flex justify-center' }">
-      <Spinner />
-    </UCard>
-    <UCard title="Colors" :ui="{ body: 'flex justify-center gap-4 flex-wrap' }">
-      <Spinner v-for="color in colors" :key="color" :color />
-    </UCard>
-    <UCard title="Sizes" :ui="{ body: 'flex justify-center items-center gap-4 flex-wrap' }">
-      <Spinner v-for="size in sizes" :key="size" :size />
-    </UCard>
+      :ui="{ container: 'py-4!' }"
+    >
+      <template #headline>
+        <UIcon name="simple-icons:heroui" class="size-12" />
+      </template>
+    </UPageHero>
+    <UPageGrid>
+      <UCard title="Usage" :ui="{ body: 'flex justify-center' }">
+        <Spinner />
+      </UCard>
+      <UCard title="Colors" :ui="{ body: 'flex justify-center gap-4 flex-wrap' }">
+        <Spinner v-for="color in colors" :key="color" :color />
+      </UCard>
+      <UCard title="Sizes" :ui="{ body: 'flex justify-center items-center gap-4 flex-wrap' }">
+        <Spinner v-for="size in sizes" :key="size" :size />
+      </UCard>
+    </UPageGrid>
   </div>
 </template>
